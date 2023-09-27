bea0964c307764fc29bfb6aecfe054cf6264a701
crisqifawei <crisqifawei@tencent.com>
2023-09-27 16:56:24 +0800

    c 文件修改

--- uart.c
+++ uart.c
@@ -3,19 +3,6 @@
 ** Created by crisqifawei 2023
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <termios.h>
-#include <errno.h>
-#include <string.h>
-#include <pthread.h>
-#include <signal.h>
-
 #include "uart.h"
 
 int rk3308_uart_open(const char* uart_device_name)
